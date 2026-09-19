c2a3f678f7e304142d2109968013360f97692b2e
galimbertip <galimberti.p@gmail.com>
2026-09-19 21:31:40 +0200

    Update CACHE_VERSION to fit-and-mind-v5.2.0

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // (Firebase, CDN): quelle passano dritte in rete, altrimenti la sync smetterebbe
 // di funzionare.
 
-const CACHE_VERSION = 'fit-and-mind-v5.1.2';
+const CACHE_VERSION = 'fit-and-mind-v5.2.0';
 const APP_SHELL = [
     './',
     './index.html',
